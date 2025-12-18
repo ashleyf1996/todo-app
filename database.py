a97f4c2e421c596a02a41a88241179a01b281c34
Ashley Fitzgerald <ashleyf1996@gmail.com>
2025-12-18 18:51:46 +0000

    FEAT: update database name, refactor main app and create users table

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 #engine to open a connection inside engine- args to pass into engine to connect to db. false by default thread. however fastapi its normal to check multiple thread
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread' : False})
--- models.py
+++ models.py
@@ -1,15 +1,27 @@
 #record inside DB table
 #TABLE - EACH RECORD 
-from database import Base #creating this model for our db.py file 
+from database import Base #imported DB. creating this model for our db.py file . impr
 from sqlalchemy import Column, Integer, String, Boolean
 
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key= True, index= True )
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name= Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 class Todos(Base): #inherits base from db.py file
-    __tablename__ = 'todos' #sql to know what to name table in db later on 
+    __tablename__ = 'todos' #CREATE NEW TABLEsql to know what to name table in db later on 
 
     id = Column(Integer, primary_key= True, index=True) #it will be an int this col. its indexable. its unuqie. increase perm
     title = Column(String)
     description = Column(String)
-    priority = Column(Integer)
-    complete = Column(Boolean)
-
-
+    priority = Column(Integer) #these will all default as null
+    complete = Column(Boolean, default=False)
+    owner = Column(Integer, foreign_key=True)
