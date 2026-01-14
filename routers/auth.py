cb75c352beb2be7c73cc5b2ab6871321ed2c26d4
Ashley Fitzgerald <ashleyf1996@gmail.com>
2026-01-14 13:32:14 +0000

    Decode jwt token

--- routers/auth.py
+++ routers/auth.py
@@ -1,22 +1,25 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from models import Users
 from passlib.context import CryptContext
 from database import engine, SessionLocal
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt 
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt , JWTError
 
 router = APIRouter()
 
 SECRET_KEY = '31672T2167UYGEADHBDEH3GDhd83'
-ALGORITHM = 'HS256'
+ALGORITHM = 'HS256' 
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated ='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -50,6 +53,7 @@ def authenticate_user(username: str, password: str, db):
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return user
+
     
 def create_access_token(username: str, user_id: int, expires_delta: timedelta ):
     
@@ -59,6 +63,18 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta ):
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
+async def get_current_user(token : Annotated[str, Depends(oauth2_bearer)]):
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None: 
+            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail='C' \
+            'Could not validate user')
+        return {'username' : username, 'id': user_id}
+    except JWTError: 
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+    
 
 @router.post("/auth/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
@@ -76,6 +92,7 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
     return {"message": "User created successfully"}
 
+
 @router.post("/token/", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency ):
@@ -84,6 +101,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
         return 'Failed authentication'
     
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
+
     return {'access_token' : token, 'token_type' : 'bearer', "cormac": "anator"}
 
 
