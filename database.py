b97801d39484701b2a5795a8a625e2806ae270c8
Ashley Fitzgerald <ashleyf1996@gmail.com>
2026-01-25 21:33:22 +0000

    Set up mysql db and connected with app

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Ballyroan123@localhost/TodoApplicationDatabase'
+
 
 #engine to open a connection inside engine- args to pass into engine to connect to db. false by default thread. however fastapi its normal to check multiple thread
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread' : False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #db session. will be come db we want session local . we want to bind to engine we just created. autocommits are false. we wanna be incontrol 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
