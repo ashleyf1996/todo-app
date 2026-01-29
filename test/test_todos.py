15ee1939b2328629219949238ddae460d7a0b8fc
Ashley Fitzgerald <ashleyf1996@gmail.com>
2026-01-29 16:51:23 +0000

    Completed todo tests

--- test/test_todos.py
+++ test/test_todos.py
@@ -1,3 +1,5 @@
+from http.client import responses
+
 from sqlalchemy import StaticPool, create_engine, text
 from sqlalchemy.orm import sessionmaker
 from fastapi import status
@@ -89,7 +91,7 @@ def test_read_one_authenticated_not_found():
 
 def test_create_todo(test_todo):
     request_data={
-        'title': 'new',
+        'title': 'newer',
         'description': 'new description',
         'priority': 5,
         'complete': False
@@ -103,3 +105,42 @@ def test_create_todo(test_todo):
     assert model.description== request_data.get('description')
     assert model.priority == request_data.get('priority')
     assert model.complete == request_data.get('complete')
+
+def test_update_todo(test_todo):
+    request_data={
+        'title': 'new title mate',
+        'description': 'new description mate',
+        'priority': 5,
+        'complete': False
+    }
+
+    response = client.put('/todo/1', json=request_data)
+    assert response.status_code == 204
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model.title == 'new title mate'
+
+def test_update_todo_not_found(test_todo):
+    request_data={
+        'title': 'new title mate',
+        'description': 'new description mate',
+        'priority': 5,
+        'complete': False
+    }
+
+    response = client.put('/todo/999', json=request_data)
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
+
+
+def test_delete_todo(test_todo):
+    response = client.delete('/todo/1')
+    assert response.status_code == 204
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+def test_delete_todo_not_found(test_todo):
+    response = client.delete('/todo/999')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
